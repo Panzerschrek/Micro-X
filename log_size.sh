fe2049cef66a33d0d11790a766ed2f5e70b069e6
Panzerschrek <AntiPanzerschrek@yandex.ru>
2016-02-06 16:31:24 +0700

    V log razmera pišetsä jescö razmer fajla, sžatogo upakovscikom kkrunchy.

--- log_size.sh
+++ log_size.sh
@@ -1,6 +1,10 @@
 rm Micro-X-upx.exe
 /C/lnk/upx391w/upx.exe Micro-X.exe -o Micro-X-upx.exe -9 --brute --ultra-brute --best --no-align
+/C/lnk/kkrunchy_023a/kkrunchy.exe Micro-X.exe --good --out Micro-X-kk.exe
+echo upx >> size_log.txt
 du Micro-X-upx.exe -b | cut -f 1 >> size_log.txt
+echo kkrunchy >> size_log.txt
+du Micro-X-kk.exe -b | cut -f 1 >> size_log.txt
 date +%H:%M\ %e.%m.%Y >> size_log.txt
 echo \
  >> size_log.txt
